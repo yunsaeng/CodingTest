f9e00fcb628f326e3746114e24bcde1e716526f2
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2023-12-24 18:46:45 +0900

    [Silver III] Title: N과 M (7), Time: 684 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define MAX 8
+
+using namespace std;
+
+int M, N, num;
+int arr[MAX] = {0,};
+vector<int> v;
+
+void BT(int cnt) {
+    if(cnt == M) {
+        for(int i=0; i<M; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for(int i=0; i<N; i++) {
+            arr[cnt] = v[i];
+            BT(cnt+1);
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for(int i=1; i<=N; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    BT(0);
+}
