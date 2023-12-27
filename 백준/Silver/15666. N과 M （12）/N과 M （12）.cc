8811a4761458ffd6a376625bc6eed5d4005913f7
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2023-12-27 17:43:04 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define MAX 8
+
+using namespace std;
+
+int M, N, num;
+int arr[MAX] = {0,};
+int number[MAX] = {0,};
+
+void BT(int cnt, int minIndex) {
+    if(cnt == M) {
+        for(int i=0; i<M; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    int tmp = 0;
+    for(int i=minIndex; i<N; i++) {
+        if(tmp != number[i]) {
+            arr[cnt] = number[i];
+            tmp = number[i];
+            BT(cnt+1, i);
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for(int i=0; i<N; i++) {
+        cin >> number[i];
+    }
+    sort(number, number+N);
+    BT(0, 0);
+}
