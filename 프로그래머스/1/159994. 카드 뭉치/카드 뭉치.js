cfb87aac5f8bbaf5c98b31f74ab41e6762a89b9d
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-10-18 12:21:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,17 +1,9 @@
 function solution(cards1, cards2, goal) {
-    let answer = '';
-    let isMake = true;
-    
-    for(word of goal) {
-        if(word === cards1[0]) cards1.shift();
-        else if (word === cards2[0]) cards2.shift();
-        else {
-            isMake = false;
-        };
-    }
-    
-    if(isMake) answer = "Yes";
-    else answer = "No";
-    
-    return answer;
+  for (const word of goal) {
+    if (word === cards1[0]) cards1.shift();
+    else if (word === cards2[0]) cards2.shift();
+    else return "No";
+  }
+
+  return "Yes";
 }
