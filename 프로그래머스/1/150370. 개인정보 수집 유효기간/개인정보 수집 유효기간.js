1b365d96c9fb08aec67fedfb5944e842ceb50518
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-10-18 13:48:42 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,7 +1,8 @@
 function solution(today, terms, privacies) {
   let answer = [];
   let expiresIn = {};
-  const [t_year, t_month, t_day] = today.split(".").map(Number);
+  const [year, month, day] = today.split(".").map(Number);
+  const today_stamp = year * 12 * 28 + month * 28 + day;
 
   terms.forEach((term) => {
     const [condition, month] = term.split(" ");
@@ -9,28 +10,12 @@ function solution(today, terms, privacies) {
   });
 
   privacies.forEach((privacy, index) => {
-    const [datePart, p_condition] = privacy.split(" ");
-    const [p_year, p_month, p_day] = datePart.split(".").map(Number);
+    let [date, condition] = privacy.split(" ");
+    date = date.split(".").map(Number);
+    const expires_stamp =
+      date[0] * 12 * 28 + date[1] * 28 + date[2] + expiresIn[condition] * 28;
 
-    let month = p_month + expiresIn[p_condition];
-    let year = p_year;
-
-    if (month > 12) {
-      year += Math.floor(month / 12);
-      month %= 12;
-      if (month === 0) {
-        month = 12;
-        year--;
-      }
-    }
-
-    if (year < t_year) answer.push(index + 1);
-    else if (year === t_year) {
-      if (month < t_month) answer.push(index + 1);
-      else if (month === t_month) {
-        if (p_day <= t_day) answer.push(index + 1);
-      }
-    }
+    if (today_stamp >= expires_stamp) answer.push(index + 1);
   });
   return answer;
 }
