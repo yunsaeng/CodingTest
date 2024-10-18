d93e9fa7505f78cc4f5b8fd7484149259097bf05
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-10-18 13:00:52 +0900

    [level 1] Title: 둘만의 암호, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,16 +1,12 @@
 function solution(s, skip, index) {
-  let possibleAlpha = Array.from({ length: 26 }, (_, i) => 
+  let alphabet = Array.from({ length: 26 }, (_, i) =>
     String.fromCharCode(i + 97)
   ).filter((alpha) => !skip.includes(alpha));
 
   let answer = [...s];
-  answer.forEach((alpha, idx) => {
-    let currentIndex = possibleAlpha.indexOf(alpha);
-    answer[idx] =
-      possibleAlpha[
-        (currentIndex + index) % possibleAlpha.length
-      ];
-  });
-
-  return answer.join("");
+  return answer
+    .map(
+      (alpha) => alphabet[(alphabet.indexOf(alpha) + index) % alphabet.length]
+    )
+    .join("");
 }
