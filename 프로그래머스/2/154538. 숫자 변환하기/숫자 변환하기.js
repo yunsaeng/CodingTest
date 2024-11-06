3487c6af9cff8678b9ec816b558938ed8e898a96
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-11-06 21:40:56 +0900

    [level 2] Title: 숫자 변환하기, Time: 32.94 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,22 @@
+function solution(x, y, n) {
+  if (x === y) return 0;
+
+  const dp = Array(y + 1).fill(Infinity);
+  dp[x] = 0;
+
+  for (let i = x; i <= y; i++) {
+    if (dp[i] === Infinity) continue;
+
+    if (i * 2 <= y) {
+      dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+    }
+    if (i * 3 <= y) {
+      dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+    }
+    if (i + n <= y) {
+      dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+    }
+  }
+
+  return dp[y] === Infinity ? -1 : dp[y];
+}
