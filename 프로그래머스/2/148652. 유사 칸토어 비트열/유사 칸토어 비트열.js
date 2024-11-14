5e2fbbd358982983ffea7e0faaf5b6fbb604de6a
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-11-15 01:27:54 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 7272.07 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -1,12 +1,5 @@
-const isOne = (num) => {
-  if (num < 5 && num !== 2) return true;
-  if (num % 5 === 2) return false;
-
-  return isOne(Math.floor(num / 5));
-};
-
 function solution(n, l, r) {
   let answer = 0;
-  for (let i = l - 1; i < r; i++) if (isOne(i)) answer++;
+  for (let i = l - 1; i < r; i++) if (!i.toString(5).match(2)) answer++;
   return answer;
 }
