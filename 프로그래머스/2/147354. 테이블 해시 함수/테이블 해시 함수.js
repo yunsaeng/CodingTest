42ff9d76ec519d23fdda239ff7e23dfa22679f58
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2024-11-15 02:10:52 +0900

    [level 2] Title: 테이블 해시 함수, Time: 44.22 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,16 +1,9 @@
 function solution(data, col, row_begin, row_end) {
-  const hashs = data
+  return data
     .sort((a, b) =>
       a[col - 1] === b[col - 1] ? b[0] - a[0] : a[col - 1] - b[col - 1]
     )
-    .map((d, i) => {
-      return d.reduce((acc, cur) => {
-        return acc + (cur % (i + 1));
-      }, 0);
-    });
-
-  let answer = hashs[row_begin - 1];
-  for (let i = row_begin; i < row_end; i++) answer = answer ^ hashs[i];
-
-  return answer;
+    .map((d, i) => d.reduce((acc, cur) => acc + (cur % (i + 1)), 0))
+    .slice(row_begin - 1, row_end)
+    .reduce((acc, cur) => acc ^ cur, 0);
 }
