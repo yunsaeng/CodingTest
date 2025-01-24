6c0c0f74493613965c86e12271ea7094bb29c76a
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-01-24 15:47:45 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.76 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,11 +1,7 @@
 function solution(food) {
-  let answer = "";
-  let temp = "";
-  for (let i = 1; i < food.length; i++) {
-    for (let j = 0; j < Math.floor(food[i] / 2); j++) {
-      temp += i;
-    }
-  }
-  answer = temp + "0" + temp.split("").reverse().join("");
-  return answer;
+  let res = "";
+  for (let i = 1; i < food.length; i++)
+    res += String(i).repeat(Math.floor(food[i] / 2));
+  return res + "0" + res.split("").reverse().join("");
+  // return res + '0' + [...res].reverse().join("");
 }
