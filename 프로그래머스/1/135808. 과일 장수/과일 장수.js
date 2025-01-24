cdb5b79ea946db397c509f86bed1e3011cb75cd7
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-01-24 15:32:41 +0900

    [level 1] Title: 과일 장수, Time: 288.00 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,7 +1,6 @@
 function solution(k, m, score) {
-  let answer = 0;
-  const rest = score.length % m;
-  score = score.sort((a, b) => b - a).slice(0, score.length - rest);
-  for (let i = 0; i < score.length; i += m) answer += score[i + m - 1] * m;
-  return answer;
+  return score
+    .sort((a, b) => b - a)
+    .filter((_, idx) => idx % m === m - 1)
+    .reduce((acc, cur) => acc + cur * m, 0);
 }
