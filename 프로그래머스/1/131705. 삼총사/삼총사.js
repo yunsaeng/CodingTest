907cc5cd00a96b855d0c09f9a416045a539f1037
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-03 17:23:46 +0900

    [level 1] Title: 삼총사, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,11 +1,16 @@
 function solution(number) {
   let answer = 0;
-  for (let i = 0; i < number.length - 2; i++) {
-    for (let j = i + 1; j < number.length - 1; j++) {
-      for (let k = j + 1; k < number.length; k++) {
-        if (number[i] + number[j] + number[k] === 0) answer++;
-      }
+
+  const combination = (current, index) => {
+    if (current.length === 3) {
+      answer += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
     }
-  }
+
+    for (let i = index; i < number.length; i++)
+      combination([...current, number[i]], i + 1);
+  };
+
+  combination([], 0);
   return answer;
 }
