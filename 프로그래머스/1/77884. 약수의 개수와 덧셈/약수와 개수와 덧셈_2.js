78837d945bc99febcf4b8fa855b853b90d0d6519
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-12 20:30:40 +0900

    Update 약수와 개수와 덧셈_2.js

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수와 개수와 덧셈_2.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수와 개수와 덧셈_2.js
@@ -1,20 +1,8 @@
 function solution(left, right) {
   let answer = 0;
-  const getDivisor = (n) => {
-    let count = 0;
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-        if (i === Math.sqrt(n)) count++;
-        else count += 2;
-      }
-    }
-    return count;
-  };
-
   for (let num = left; num <= right; num++) {
-    if (getDivisor(num) % 2 === 0) answer += num;
-    else answer -= num;
+    if (Number.isInteger(Math.sqrt(num))) answer -= num;
+    else answer += num;
   }
-
   return answer;
 }
