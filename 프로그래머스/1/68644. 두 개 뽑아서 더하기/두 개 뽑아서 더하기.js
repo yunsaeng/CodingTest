1f5856ed3f77ace0262b2a97127052378d5d2947
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-13 18:43:29 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.63 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const result = new Set();
+    for(let i = 0; i < numbers.length; i++) {
+        for(let j = i + 1; j < numbers.length; j++) {
+            result.add(numbers[i] + numbers[j]);
+        }
+    }
+    return [...result].sort((a, b) => a - b);
+}
