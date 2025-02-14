83cd6185382c627dd2a2222798b501163812ff3e
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-15 03:34:14 +0900

    [level 1] Title: 실패율, Time: 4852.96 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,23 +1,10 @@
 function solution(N, stages) {
   const answer = [];
-
-  stages = stages.sort((a, b) => a - b);
   for (let i = 1; i <= N; i++) {
-    const total = stages.length;
-    // if (total === 0) {
-    //   answer.push([i, 0]);
-    //   continue;
-    // }
-
-    let rest = 0;
-    while (stages.length > 0 && stages[0] === i) {
-      stages.shift();
-      rest++;
-    }
+    const total = stages.filter((stage) => stage >= i).length;
+    const rest = stages.filter((stage) => stage === i).length;
     answer.push([i, rest / total]);
   }
 
-  return answer
-    .sort((a, b) => (b[1] === a[1] ? a[0] - b[0] : b[1] - a[1]))
-    .map((e) => e[0]);
+  return answer.sort((a, b) => b[1] - a[1]).map((e) => e[0]);
 }
