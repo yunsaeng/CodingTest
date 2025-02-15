5f290775cc7f8b1ec8a6f3bd79cf313710db6be2
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-16 02:34:58 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,9 @@
 function solution(array, commands) {
-  const answer = [];
-  commands.forEach((command) => {
+  return commands.map((command) => {
     const [start, end, index] = command;
     const value = [...array]
       .splice(start - 1, end - start + 1)
       .sort((a, b) => a - b)[index - 1];
-    answer.push(value);
+    return value;
   });
-  return answer;
 }
