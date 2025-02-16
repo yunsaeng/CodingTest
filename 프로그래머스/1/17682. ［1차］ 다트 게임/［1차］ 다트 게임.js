086add9c5a658d8792d0bdd7412fbd27908c8a6b
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-17 04:48:40 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,21 @@
+function solution(dartResult) {
+  const answer = [];
+  const points = dartResult.match(/\d+/g).map((point) => Number(point));
+  const events = dartResult.match(/(?<=\d)([A-Z\W+][\W]*)/g);
+  points.forEach((point, index) => {
+    const event = events[index];
+    const [bonus, option] = event.split("");
+
+    if (bonus === "S") answer.push(point);
+    else if (bonus === "D") answer.push(Math.pow(point, 2));
+    else if (bonus === "T") answer.push(Math.pow(point, 3));
+
+    if (option === "#") answer[index] *= -1;
+    else if (option === "*") {
+      answer[index] *= 2;
+      if (index > 0) answer[index - 1] *= 2;
+    }
+  });
+
+  return answer.reduce((acc, cur) => acc + cur, 0);
+}
