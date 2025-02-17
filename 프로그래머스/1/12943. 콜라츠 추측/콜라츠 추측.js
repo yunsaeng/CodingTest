e6876fe217a330f8578bf7bcbd6a2795852ef5c6
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-17 19:22:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -2,8 +2,7 @@ function solution(num) {
   let answer = 0;
   while (num !== 1) {
     if (answer > 500) return -1;
-    if (num % 2 === 0) num /= 2;
-    else num = num * 3 + 1;
+    num % 2 === 0 ? (num /= 2) : (num = num * 3 + 1);
     answer++;
   }
   return answer;
