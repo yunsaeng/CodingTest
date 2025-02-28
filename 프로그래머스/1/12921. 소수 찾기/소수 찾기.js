3ce9b7ba55b9de232c54b3dcf85e50a447b9bc1c
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-02-28 16:51:22 +0900

    [level 1] Title: 소수 찾기, Time: 51.15 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -2,9 +2,9 @@
      const prime = Array(n + 1).fill(true);
      prime[0] = prime[1] = false;
      
-     for(let i = 2; i <= n; i++) {
+     for(let i = 2; i * i <= n; i++) {
          if(prime[i]) {
-             for(let j = i * 2; j <= n; j+=i) {
+             for(let j = i * i; j <= n; j+=i) {
                  prime[j] = false;
              }
          }
