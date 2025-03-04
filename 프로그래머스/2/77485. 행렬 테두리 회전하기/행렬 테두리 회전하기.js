8fb2ed43b70b99d448c6131a29f8f439645029ff
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-03-04 17:52:21 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 31.84 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -1,43 +1,33 @@
 function solution(rows, columns, queries) {
-  const answer = [];
-
   const arr = Array.from({ length: rows }, (_, r) =>
     Array.from({ length: columns }, (_, c) => r * columns + c + 1)
   );
+  return queries.map((query) => {
+    const [sr, sc, er, ec] = query.map((e) => e - 1);
+    let min = arr[sr][sc];
+    const temp = arr[sr][sc];
 
-  queries.forEach((query) => {
-    const [sr, sc, er, ec] = query;
-    let min = Number.MAX_SAFE_INTEGER;
-    for (let i = sr - 1; i < er - 1; i++) {
-      min = Math.min(min, Math.min(arr[i][sc - 1], arr[i + 1][sc - 1]));
-      [arr[i][sc - 1], arr[i + 1][sc - 1]] = [
-        arr[i + 1][sc - 1],
-        arr[i][sc - 1],
-      ];
+    for (let i = sr; i < er; i++) {
+      arr[i][sc] = arr[i + 1][sc];
+      min = Math.min(min, arr[i][sc]);
     }
-    for (let i = sc - 1; i < ec - 1; i++) {
-      min = Math.min(min, Math.min(arr[er - 1][i], arr[er - 1][i + 1]));
-      [arr[er - 1][i], arr[er - 1][i + 1]] = [
-        arr[er - 1][i + 1],
-        arr[er - 1][i],
-      ];
+
+    for (let i = sc; i < ec; i++) {
+      arr[er][i] = arr[er][i + 1];
+      min = Math.min(min, arr[er][i]);
     }
-    for (let i = er - 1; i > sr - 1; i--) {
-      min = Math.min(min, Math.min(arr[i][ec - 1], arr[i - 1][ec - 1]));
-      [arr[i][ec - 1], arr[i - 1][ec - 1]] = [
-        arr[i - 1][ec - 1],
-        arr[i][ec - 1],
-      ];
+
+    for (let i = er; i > sr; i--) {
+      arr[i][ec] = arr[i - 1][ec];
+      min = Math.min(min, arr[i][ec]);
     }
-    for (let i = ec - 1; i > sc; i--) {
-      min = Math.min(min, Math.min(arr[sr - 1][i], arr[sr - 1][i - 1]));
-      [arr[sr - 1][i], arr[sr - 1][i - 1]] = [
-        arr[sr - 1][i - 1],
-        arr[sr - 1][i],
-      ];
+
+    for (let i = ec; i > sc; i--) {
+      arr[sr][i] = arr[sr][i - 1];
+      min = Math.min(min, arr[sr][i]);
     }
-    
-    answer.push(min);
+
+    arr[sr][sc + 1] = temp;
+    return min;
   });
-  return answer;
 }
