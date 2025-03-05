205988b4e339f8e88ad5105ac19756ec220dd68a
윤생 <30286303+yoonsaeng@users.noreply.github.com>
2025-03-05 23:46:23 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 214.42 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
@@ -1,13 +1,6 @@
 function solution(w, h) {
-  const getGCD = (a, b) => {
-    if (b === 0) return a;
-    return getGCD(b, a % b);
-  };
-  const gcd = getGCD(Math.max(w, h), Math.min(w, h));
-  const miniW = w / gcd;
-  const miniH = h / gcd;
-  let answer = miniH * miniW;
-  for (let y = 1; y <= miniH; y++)
-    answer -= (miniW - Math.ceil((miniW * y) / miniH)) * 2;
-  return w * h - answer * gcd;
+    let answer = 0;
+    for(let x = 1; x <= w; x++)
+        answer += Math.ceil(x * (h / w));
+    return (w * h - answer) * 2;
 }
