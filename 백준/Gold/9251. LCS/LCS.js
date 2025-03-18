31cebb985bba273b72876907b8a3385a33b025a9
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-03-19 03:58:50 +0900

    [Gold V] Title: LCS, Time: 216 ms, Memory: 25016 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출시 활성화
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트할 때 파일을 통해 입력 받음
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  let answer = 0;
+  const [str1, str2] = input;
+  const len1 = str1.length;
+  const len2 = str2.length;
+  const dp = Array.from({ length: len1 + 1 }, () => Array(len2 + 1).fill(0));
+
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  console.log(dp[len1][len2]);
+}
