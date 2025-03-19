1d9a188bb86bedd94e7d2075c103c90f57e541be
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-03-19 19:33:35 +0900

    [Bronze II] Title: 벌집, Time: 164 ms, Memory: 9716 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출시 활성화
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트할 때 파일을 통해 입력 받음
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  const N = +input[0];
+  let cur = 1;
+  let mul = 1;
+  while (N > cur) {
+    cur += mul * 6;
+    mul++;
+  }
+
+  console.log(mul);
+}
