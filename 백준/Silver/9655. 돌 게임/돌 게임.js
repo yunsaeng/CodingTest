b948ec74d770a12747236fdb0c2b0b48898ef6b5
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-05-02 15:16:05 +0900

    [Silver V] Title: 돌 게임, Time: 168 ms, Memory: 9508 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출 시
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트용
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  const N = +input[0];
+  if (N % 2 === 0) console.log("CY");
+  else console.log("SK");
+}
