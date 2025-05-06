f62b759690bec00945bd0b5091b4db0261e3bc7d
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-05-06 09:03:26 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 168 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출 시
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트용
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  const N = +input[0];
+  const seq = input[1].split(" ").map(Number);
+  const dp = Array.from({ length: N }, () => 1);
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < i; j++) {
+      if (seq[i] < seq[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  console.log(Math.max(...dp));
+}
