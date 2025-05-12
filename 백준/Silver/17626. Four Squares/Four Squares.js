4e07dea9ec638b68ce2043095c467bd5fe9f689b
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-05-12 14:58:51 +0900

    [Silver III] Title: Four Squares, Time: 220 ms, Memory: 10952 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출 시
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트용
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  const N = +input[0];
+  const dp = Array(N + 1).fill(Number.MAX_SAFE_INTEGER);
+  dp[0] = 0;
+
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+
+  console.log(dp[N]);
+}
