df12ef455e208c42a65913d0773fcbd7cdb528bf
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-05-13 13:11:56 +0900

    [Silver II] Title: 상자넣기, Time: 168 ms, Memory: 9828 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출 시
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트용
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  const N = +input[0];
+  const box = input[1].split(" ").map(Number);
+  const dp = Array(N).fill(1);
+
+  for (let i = 1; i < N; i++) {
+    for (let j = 0; j < i; j++) {
+      if (box[j] < box[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+
+  console.log(Math.max(...dp));
+}
