965c8f3944507e0ed7975d1a54ea210d40faa3a8
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-05-18 17:18:06 +0900

    [Silver II] Title: 병사 배치하기, Time: 180 ms, Memory: 9916 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출 시
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트용
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  const N = +input[0];
+  const soldiers = input[1].split(" ").map(Number);
+  const dp = Array(N).fill(1);
+  for (let i = 1; i < N; i++) {
+    for (let j = 0; j < i; j++) {
+      if (soldiers[j] > soldiers[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  console.log(N - Math.max(...dp));
+}
