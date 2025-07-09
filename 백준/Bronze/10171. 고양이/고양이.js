3b35c16f46241112202c2989fad1d5a157d061a1
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-07-09 15:09:16 +0900

    [Bronze V] Title: 고양이, Time: 104 ms, Memory: 9444 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin, // 백준 제출 시
+  // input: fs.createReadStream("./example.txt"), // 로컬 테스트용
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`);
+}
