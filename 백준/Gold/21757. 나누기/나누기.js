631cdacca48dafab87c1c490386ff0cf22370459
윤생 <30286303+yunsaeng@users.noreply.github.com>
2025-10-24 13:17:32 +0900

    [Gold II] Title: 나누기, Time: 1184 ms, Memory: 35372 KB, Score: 60 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.js
+++ 백준/Gold/21757. 나누기/나누기.js
@@ -8,28 +8,34 @@ const input = fs.readFileSync(inputPath).toString().trim().split("\n");
 function solve() {
   const it = input.values();
   const N = it.next().value;
-  const sequence = it.next().value.split(" ").map(Number);
+  const sequence = it.next().value.split(" ").map(BigInt);
 
-  const sum = Array(N).fill(0);
+  const sum = Array(N).fill(0n);
   sum[0] = sequence[0];
   for (let i = 1; i < N; i++) {
     sum[i] = sum[i - 1] + sequence[i];
   }
 
-  let result = 0;
-  for (let i = 0; i < N - 3; i++) {
-    for (let j = i + 1; j < N - 2; j++) {
-      for (let k = j + 1; k < N - 1; k++) {
-        const quarter1 = sum[i];
-        const quarter2 = sum[j] - sum[i];
-        const quarter3 = sum[k] - sum[j];
-        const quarter4 = sum[N - 1] - sum[k];
-        if(quarter1 === quarter2 && quarter2 === quarter3 && quarter3 === quarter4) result++;
-      }
+  let result = 0n;
+  for (let i = 1; i < N - 2; i++) {
+    if (sum[i] !== sum[N - 1] - sum[i]) continue;
+
+    let temp1 = 0n;
+    for (let j = 0; j < i; j++) {
+      if (sum[j] !== sum[i] - sum[j]) continue;
+      temp1 += 1n;
     }
+
+    let temp2 = 0n;
+    for (let k = i + 1; k < N - 1; k++) {
+      if (sum[k] - sum[i] !== sum[N - 1] - sum[k]) continue;
+      temp2 += 1n;
+    }
+
+    result += temp1 * temp2;
   }
-  
-  return result;
+
+  return result.toString();
 }
 // 결과 출력
 console.log(solve());
