77a398702b15567226e35affa96931652321062b
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-21 15:14:59 +0900

    [Passed] 점수 입출력 (136ms, 10MB)

---
+++ trail0/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.printf("Your score is %d point.", N);
+    }
+}
