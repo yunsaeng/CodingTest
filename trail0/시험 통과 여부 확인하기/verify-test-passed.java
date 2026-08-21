36121c25ebc97219d90cf0f624dc27b10115aab3
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-21 15:27:24 +0900

    [Passed] 시험 통과 여부 확인하기 (125ms, 10MB)

---
+++ trail0/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(N >= 80 ? "pass" : (80 - N) + " more score");
+    }
+}
