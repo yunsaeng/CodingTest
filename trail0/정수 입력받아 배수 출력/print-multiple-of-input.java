9de72ac63d466137ed7a27b26a427654e2e4f9c3
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-21 15:43:19 +0900

    [Passed] 정수 입력받아 배수 출력 (129ms, 10MB)

---
+++ trail0/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 1; i <= 5; i++) System.out.print((N * i) + " ");
+    }
+}
