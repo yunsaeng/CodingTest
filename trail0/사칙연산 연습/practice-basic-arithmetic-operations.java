d70237630f6cc51b0bf86aea295ecb04a6d3162d
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-21 15:13:52 +0900

    [Passed] 사칙연산 연습 (89ms, 9MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 30, c = a + b;
+        System.out.println(c / 8);
+        System.out.println(c % 8);
+    }
+}
