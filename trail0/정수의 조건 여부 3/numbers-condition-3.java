91a8b73f9d5c43071b91f116714cdc7f1d30acf7
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-22 11:59:26 +0900

    [Passed] 정수의 조건 여부 3 (124ms, 10MB)

---
+++ trail0/정수의 조건 여부 3/numbers-condition-3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println(a % 13 == 0 || a % 19 == 0 ? "True" : "False");
+    }
+}
