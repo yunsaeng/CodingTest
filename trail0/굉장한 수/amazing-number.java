b86b3853037d053b69423520989efe08b0a9c873
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-22 20:13:06 +0900

    [Passed] 굉장한 수 (125ms, 10MB)

---
+++ trail0/굉장한 수/amazing-number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println((N % 2 == 1 && N % 3 == 0) || (N % 2 == 0 && N % 5 == 0));
+    }
+}
