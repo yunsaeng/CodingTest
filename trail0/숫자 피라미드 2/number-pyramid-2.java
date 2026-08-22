fbece1ed9eaae93750d3f37d3e65f646b312db41
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-22 22:17:56 +0900

    [Passed] 숫자 피라미드 2 (130ms, 10MB)

---
+++ trail0/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cur = 0;
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j <= i; j++) {
+                cur++;
+                System.out.print(cur + " ");
+            }
+            System.out.println();
+        }
+    }
+}
