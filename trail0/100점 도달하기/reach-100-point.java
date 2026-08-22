d650bdcf8aa2f23ec549c972421471c4fcba3705
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-22 20:28:41 +0900

    [Passed] 100점 도달하기 (124ms, 10MB)

---
+++ trail0/100점 도달하기/reach-100-point.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = N; i <= 100; i++) {
+            if(i >= 90) System.out.print("A ");
+            else if(i >= 80) System.out.print("B ");
+            else if(i >= 70) System.out.print("C ");
+            else if(i >= 60) System.out.print("D ");
+            else System.out.print("F ");
+        }
+    }
+}
