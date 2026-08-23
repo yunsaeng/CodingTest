40f1e69d1eb1991cfae6b5768afbff9340064bf2
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-23 13:29:28 +0900

    [Passed] 두 배열의 곱 (131ms, 10MB)

---
+++ trail0/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr1 = new int[3][3];
+        int[][] arr2 = new int[3][3];
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print(arr1[i][j] * arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
