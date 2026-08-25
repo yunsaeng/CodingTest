c0364b8cf3eb09f1e46c836415e2530efb3abfe6
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-25 13:31:19 +0900

    [Passed] 합을 복사하기 (98ms, 9MB)

---
+++ trail1/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3;
+
+        a += b + c;
+        b = a;
+        c = a;
+
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
