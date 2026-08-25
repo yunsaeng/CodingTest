25d47818abee646cae363e6520853ce8a4214b08
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-25 17:15:07 +0900

    [Passed] 문자열 순서 바꾸기 (126ms, 10MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next(), T = sc.next();
+        System.out.println(T);
+        System.out.println(S);
+    }
+}
