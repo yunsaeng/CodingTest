f20a9a636005a913d3127a8e495a1576329bf5cb
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 11:38:16 +0900

    [Passed] 정사각형의 넓이 (138ms, 10MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N * N);
+        if(N < 5) System.out.println("tiny");
+    }
+}
