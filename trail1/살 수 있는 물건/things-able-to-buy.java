ce03f26eaff5d1053b31f20ba700c9afe829b471
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 12:00:06 +0900

    [Passed] 살 수 있는 물건 (124ms, 10MB)

---
+++ trail1/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if(N >= 3000) System.out.println("book");
+        else if(N >= 1000) System.out.println("mask");
+        else System.out.println("no");
+    }
+}
