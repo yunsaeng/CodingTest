22733bfd80f13ef21acaab5e2648049cabfd8d6a
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 14:58:02 +0900

    [Passed] 남녀노소 구분짓기 (127ms, 10MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int gender = sc.nextInt(), age = sc.nextInt();
+        System.out.println(gender == 0 ? (age >= 19 ? "MAN" : "BOY") : (age >= 19 ? "WOMAN" : "GIRL"));
+    }
+}
