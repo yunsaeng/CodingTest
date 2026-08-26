47d5880e18ff0b3985759c66684c39b6ea39217e
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 14:02:13 +0900

    [Passed] 특정 조건 세 정수 비교 (127ms, 10MB)

---
+++ trail1/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        System.out.printf("%d %d", a <= b && a <= c ? 1 : 0, a == b & a == c ? 1 : 0);
+    }
+}
