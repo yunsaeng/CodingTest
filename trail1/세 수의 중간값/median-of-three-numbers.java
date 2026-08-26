bfaec9fa0f3886ea8b5d6903e68e8621d4560067
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 14:05:41 +0900

    [Passed] 세 수의 중간값 (124ms, 10MB)

---
+++ trail1/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt(), C = sc.nextInt();
+        System.out.println(B > A && B < C ? 1 : 0);
+    }
+}
