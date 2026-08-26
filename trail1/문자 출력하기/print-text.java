4aa31fdef52dcedcce572f4387ce6ba9ecea437c
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 19:20:06 +0900

    [Passed] 문자 출력하기 (130ms, 10MB)

---
+++ trail1/문자 출력하기/print-text.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        for(int i = 0; i < 8; i++) System.out.print(s);
+    }
+}
