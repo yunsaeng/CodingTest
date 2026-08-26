a42d5c2d58a0be3d96df4340923102e7f31e0413
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 19:16:40 +0900

    [Passed] n부터 1까지 정수 출력 (128ms, 10MB)

---
+++ trail1/n부터 1까지 정수 출력/print-n-to-1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n >= 1) {
+            System.out.print(n + " ");
+            n--;
+        }
+    }
+}
