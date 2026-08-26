310d03082693ae3fa5165bde543ab4b021bf91dd
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 18:24:26 +0900

    [Passed] 증가시키며 출력하기 2 (90ms, 9MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        while(n <= 26) {
+            System.out.print(n + " ");
+            n++;
+        }
+    }
+}
