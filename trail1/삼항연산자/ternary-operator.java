5ed6e91868bf5137cfee82b17f8f558c129cf578
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 11:52:48 +0900

    [Passed] 삼항연산자 (128ms, 10MB)

---
+++ trail1/삼항연산자/ternary-operator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        if(score == 100) System.out.println("pass");
+        else System.out.println("failure");
+    }
+}
