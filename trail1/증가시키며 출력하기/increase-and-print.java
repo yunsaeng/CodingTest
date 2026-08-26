964f04fa20dd426de9d75b68e9bf3e79f7e14ef9
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-26 18:00:51 +0900

    [Passed] 증가시키며 출력하기 (107ms, 10MB)

--- trail1/증가시키며 출력하기/increase-and-print.java
+++ trail1/증가시키며 출력하기/increase-and-print.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         for(int i = 5; i <= 17; i+=2) System.out.print(i + " ");
     }
 }
