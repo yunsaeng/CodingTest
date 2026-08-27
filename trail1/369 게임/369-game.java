d53e1f2e1ecaf5618f2ac02cd2bb6d73f46c3010
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-27 17:24:00 +0900

    [Passed] 369 게임 (134ms, 10MB)

--- trail1/369 게임/369-game.java
+++ trail1/369 게임/369-game.java
@@ -5,7 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         for(int i = 1; i <= N; i++) {
-            System.out.printf("%d ", i % 3 == 0 || String.valueOf(i).matches(".*[369].*") ? 0 : i);
+            String s = String.valueOf(i);
+            System.out.printf("%d ", i % 3 == 0 || s.contains("3") || s.contains("6") || s.contains("9") ? 0 : i);
         }
     }
 }
