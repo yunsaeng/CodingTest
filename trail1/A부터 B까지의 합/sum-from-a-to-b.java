71895bc6619670280568bab8acffd14e215f2d48
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-28 16:40:58 +0900

    [Passed] A부터 B까지의 합 (128ms, 10MB)

---
+++ trail1/A부터 B까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt();
+        int sum = 0;
+        for(int i = A; i <= B; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
