eec7effab3059345fadca831a39ae062ad8f0df1
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-29 17:03:19 +0900

    [Passed] 별표 출력하기 (130ms, 10MB)

---
+++ trail1/별표 출력하기/print-star.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = i; j < N - 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
