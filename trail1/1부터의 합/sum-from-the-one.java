6a52f0c4e6c674f4db5611d9b0fd112c3adbac8d
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-29 12:45:29 +0900

    [Passed] 1부터의 합 (131ms, 10MB)

---
+++ trail1/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= 100; i++) {
+            sum += i;
+            if(sum >= N) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
