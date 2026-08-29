ef676ec9fb2157cbea89c7d337c0eebb3514da46
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-29 11:50:51 +0900

    [Passed] A의 배수들의 곱 (135ms, 10MB)

---
+++ trail1/A의 배수들의 곱/product-of-multiples-of-a.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt();
+        int prod =1;
+        for(int i = 1; i <= B; i++) {
+            if(i % A == 0) {
+                prod *= i;
+            }
+        }
+        System.out.println(prod);
+    }
+}
