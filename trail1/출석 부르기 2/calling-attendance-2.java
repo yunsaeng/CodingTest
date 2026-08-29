8556459677d91ef7572efdbc4ace3d03164d1381
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-29 12:57:49 +0900

    [Passed] 출석 부르기 2 (125ms, 10MB)

---
+++ trail1/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int n = sc.nextInt();
+            if(n == 1) System.out.println("John");
+            else if(n == 2) System.out.println("Tom");
+            else if(n == 3) System.out.println("Paul");
+            else if(n == 4) System.out.println("Sam");
+            else {
+                System.out.println("Vacancy");
+                break;
+            }
+        }
+    }
+}
