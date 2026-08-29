ece1d3df2a7b0482f47d28720cb118c163e81dca
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-29 11:29:26 +0900

    [Passed] A부터 B까지 짝수의 합 (123ms, 10MB)

---
+++ trail1/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt();
+        int sum = 0;
+        for(int i = A; i <= B; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
