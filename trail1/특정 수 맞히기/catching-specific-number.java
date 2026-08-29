93ba524fa8c917086457bc05214fe0a3be92e86c
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-29 12:55:42 +0900

    [Passed] 특정 수 맞히기 (125ms, 10MB)

---
+++ trail1/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int n = sc.nextInt();
+            if(n > 25) System.out.println("Lower");
+            else if(n < 25) System.out.println("Higher");
+            else {
+                System.out.println("Good");
+                break;
+            }
+        }
+    }
+}
