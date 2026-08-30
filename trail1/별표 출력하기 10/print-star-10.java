321e863f2f561f634e071db4619ce1fc7fbbafb6
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-08-30 13:45:03 +0900

    [Passed] 별표 출력하기 10 (133ms, 10MB)

--- trail1/별표 출력하기 10/print-star-10.java
+++ trail1/별표 출력하기 10/print-star-10.java
@@ -5,24 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int min = 0, max = N + 1;
+        
         for(int i = 1; i <= N * 2; i++) {
-            if(i <= N) {
-                if(i % 2 == 1) {
-                    min++;
-                    for(int j = 1; j <= min; j++) System.out.print("* ");
-                } else {
-                    max--;
-                    for(int j = 1; j <= max; j++) System.out.print("* ");                    
-                }
-            } else {
-                if((2 * N - i + 1) % 2 == 1) {
-                    max--;
-                    for(int j = 1; j <= max; j++) System.out.print("* ");
-                } else {
-                    min++;
-                    for(int j = 1; j <= min; j++) System.out.print("* ");
-                }
-            }
+            int stars = (i % 2 == 1) ? ++min : --max;
+            for(int j = 0; j < stars; j++) System.out.print("* ");
             System.out.println();
         }
     }
