114e12bcf4d109fc2f43b52a1cbbd2dc7cd163df
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-05 13:14:12 +0900

    [Passed] 좌우반전시킨 정사각형 (131ms, 10MB)

---
+++ trail1/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for(int i = 1; i <= N; i++) {
+            for(int j = N; j >= 1; j--) {
+                System.out.print((i * j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
