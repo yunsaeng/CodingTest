c0055d3873b0d25b492cf1e8dad0b2e5c62fc3e0
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-06 14:12:59 +0900

    [Passed] 특이한 출력 (155ms, 11MB)

---
+++ trail1/특이한 출력/special-print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= N; j++) {
+                System.out.printf("(%d, %d) ", i, j);
+                if((i + j) % 4 == 0) System.out.println();
+            }
+        }
+    }
+}
