7701ffaa08adc11f1cff4c5f60ea98bf2d044619
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-07 11:14:49 +0900

    [Passed] 배열 채우고 출력하기 (125ms, 10MB)

---
+++ trail1/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = new String[10];
+        for(int i = 0; i < 10; i++) strArr[i] = sc.next();
+        for(int i = 9; i >= 0; i--) System.out.print(strArr[i]);
+    }
+}
