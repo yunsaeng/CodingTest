e49c0243741167041bd115a4060772fa40bb2339
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-07 12:34:09 +0900

    [Passed] 특정 수 도달하기 (140ms, 10MB)

---
+++ trail1/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0, cnt = 0;
+        for(int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+            if(num >= 250) break;
+            sum += num;
+            cnt++;
+        }
+        System.out.printf("%d %.1f", sum, (double)sum / cnt);
+    }
+}
