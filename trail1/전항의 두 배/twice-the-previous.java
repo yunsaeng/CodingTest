ddb238550dd43c58ff167f320b2523621f0fc84f
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-08 12:37:49 +0900

    [Passed] 전항의 두 배 (137ms, 10MB)

---
+++ trail1/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+
+        for(int i = 2; i < 10; i++) arr[i] = arr[i - 1] + (2 * arr[i - 2]);
+        for(int i = 0; i < 10; i++) System.out.print(arr[i] + " ");
+    }
+}
