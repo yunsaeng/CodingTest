03bf4e8598082006f96f71e44b10dd7426dac1a4
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-10 11:42:11 +0900

    [Passed] 더 긴 문자열 (132ms, 10MB)

---
+++ trail1/더 긴 문자열/longer-string.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        System.out.println(s1.length() >= s2.length() 
+        ? (s1.length() == s2.length() ? "same" : (s1 + " " + s1.length())) 
+        : (s2 + " " + s2.length()));
+    }
+}
