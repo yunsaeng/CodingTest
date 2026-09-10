6d742289cfd58daae3a7cdbe9853e64883813e6b
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-10 12:21:15 +0900

    [Passed] 단어로 구분하기 (125ms, 10MB)

---
+++ trail1/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(String s : sc.nextLine().split(" ")) System.out.println(s);
+    }
+}
