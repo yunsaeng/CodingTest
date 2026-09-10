4e8a26105e97cbcad5ab1f511f26c2304ddb6fc2
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-10 13:04:06 +0900

    [Passed] 문자열 나누기 (125ms, 10MB)

---
+++ trail1/문자열 나누기/divide-string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+        String str = sc.nextLine().replaceAll(" ", "");
+        
+        for(int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+            if((i + 1) % 5 == 0) System.out.println();
+        }
+    }
+}
