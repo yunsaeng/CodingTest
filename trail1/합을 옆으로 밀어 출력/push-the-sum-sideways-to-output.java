88aa4c69fb8b63d711392e1bf36b4848793179a6
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-12 14:00:35 +0900

    [Passed] 합을 옆으로 밀어 출력 (155ms, 10MB)

---
+++ trail1/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i < N; i++) sum += sc.nextInt();
+        System.out.println(Integer.toString(sum).substring(1) + Integer.toString(sum).substring(0, 1));
+    }
+}
