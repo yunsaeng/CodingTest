ec2bae963941b6fe60675d7418b0ab34678be517
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-12 13:25:47 +0900

    [Passed] 붙여서 합하기 (121ms, 10MB)

---
+++ trail1/붙여서 합하기/add-and-add.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next(), B = sc.next();
+        System.out.println(Integer.parseInt(A + B) + Integer.parseInt(B + A));
+    }
+}
