c2b929946d85c3b9c2080a40e4e48e86c2a55f05
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-14 12:01:25 +0900

    [Passed] 함수를 이용한 소수 판별 (136ms, 10MB)

--- trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class Main {
-    public static boolean isMagicNumber(int n) {
-        int cnt = 0;
-        for(int i = 1; i <= n; i++) if(n % i == 0) cnt++;
-        return cnt == 2;
+    public static boolean isPrime(int n) {
+        if(n <= 1) return false;
+        if(n == 2) return true;
+        if(n % 2 == 0) return false;
+        for(int i = 3; i * i <= n; i += 2) {
+            if(n % i == 0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -11,7 +15,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = 0;
-        for(int n = a; n <= b; n++) if(isMagicNumber(n)) sum += n;;
+        for(int n = a; n <= b; n++) if(isPrime(n)) sum += n;;
         System.out.println(sum);
     }
 }
