390dcde56818fe794029169bc82f8608732100dc
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-15 11:29:31 +0900

    [Passed] 재귀함수의 꽃 (144ms, 10MB)

---
+++ trail2/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void print(int n) {
+        if(n == 0) return;
+        System.out.print(n + " ");
+        print(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+    }
+}
