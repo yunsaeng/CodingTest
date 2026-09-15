00b819a5a1c1c2e2a88c4ab3cff37139de4f1bee
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-15 12:19:14 +0900

    [Passed] 이상한 수열 (131ms, 10MB)

---
+++ trail2/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static int F(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return F(n / 3) + F(n - 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(F(n));
+    }
+}
