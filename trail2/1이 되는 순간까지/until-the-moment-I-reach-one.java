c9cd65729d5b4bdb25b0191efdaf7cffd2251ed1
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-15 11:39:20 +0900

    [Passed] 1이 되는 순간까지 (144ms, 10MB)

---
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static int F(int n) {
+        if(n == 1) return 0;
+
+        if(n % 2 == 0) return F(n / 2) + 1;
+        else return F(n / 3) + 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(F(n));
+    }
+}
