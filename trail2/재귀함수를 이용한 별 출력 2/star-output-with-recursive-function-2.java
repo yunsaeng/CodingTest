61544f564d73a168c5c663c19fc7c27126925e7b
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-15 11:31:03 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (251ms, 13MB)

---
+++ trail2/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void printStar(int n) {
+        if(n == 0) return;
+        for(int i = 1; i <= n; i++) System.out.print("* ");
+        System.out.println();
+        printStar(n - 1);
+        for(int i = 1; i <= n; i++) System.out.print("* ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printStar(n);
+    }
+}
