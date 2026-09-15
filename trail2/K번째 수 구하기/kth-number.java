b781f078918a993a4f25ad841ebcba239be5eb23
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-15 12:56:46 +0900

    [Passed] K번째 수 구하기 (135ms, 10MB)

---
+++ trail2/K번째 수 구하기/kth-number.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+        System.out.println(nums[k - 1]);
+    }
+}
