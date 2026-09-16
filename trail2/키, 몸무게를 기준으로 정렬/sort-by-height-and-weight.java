ac17dbce861ab0c6c72ab40c941b374430c32149
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-16 12:35:36 +0900

    [Passed] 키, 몸무게를 기준으로 정렬 (135ms, 10MB)

---
+++ trail2/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Student implements Comparable<Student>{
+    String name;
+    int height, weight;
+
+    public Student(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        if(this.height != student.height) return this.height - student.height;
+        return student.weight - this.weight;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+            students[i] = new Student(sc.next(), sc.nextInt(), sc.nextInt());
+        }
+
+        Arrays.sort(students);
+        for(Student student : students) System.out.printf("%s %d %d\n", student.name, student.height, student.weight);
+    }
+}
