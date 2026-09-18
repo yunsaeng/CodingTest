14dd16364468f57dad617c0e6e0a0468606dfd75
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-18 12:11:20 +0900

    [Passed] 선두를 지켜라 (250ms, 22MB)

---
+++ trail2/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    static final int MAX_TIME = 1000001;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int totalTime = 0;
+
+        int[] A = new int[MAX_TIME];
+        for (int i = 0; i < n; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for(int j = totalTime + 1; j <= totalTime + t; j++) {
+                A[j] = A[j - 1] + v;
+            }
+            totalTime += t;
+        }
+
+        int[] B = new int[MAX_TIME];
+        totalTime = 0;
+        for (int i = 0; i < m; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for(int j = totalTime + 1; j <= totalTime + t; j++) {
+                B[j] = B[j - 1] + v;
+            }
+            totalTime += t;
+        }
+
+        int cnt = 0;
+        String head = null;
+        for(int t = 1; t <= totalTime; t++) {
+            if(A[t] > B[t]) {
+                if(head != null && head.equals("B")) cnt++;
+                head = "A";
+            } else if(A[t] < B[t]) {
+                if(head != null && head.equals("A")) cnt++; 
+                head = "B";
+            }
+        }
+
+        System.out.println(cnt);
+        sc.close();
+    }
+}
