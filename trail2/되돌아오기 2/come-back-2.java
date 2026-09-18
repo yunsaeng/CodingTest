1ad52265ed704e238cca888d74b3caa2f152bce4
윤생 <30286303+yunsaeng@users.noreply.github.com>
2026-09-18 14:09:52 +0900

    [Passed] 되돌아오기 2 (215ms, 14MB)

---
+++ trail2/되돌아오기 2/come-back-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] commands = sc.next().toCharArray();
+        int[] dirX = {0, 1, 0, -1};
+        int[] dirY = {1, 0, -1, 0};
+        int x = 0, y = 0, curDirIdx = 0, time = 0;
+
+        for(char c : commands) {
+            time++;
+
+            if(c == 'L') curDirIdx = (curDirIdx + 3) % 4;
+            else if(c == 'R') curDirIdx = (curDirIdx + 1) % 4;
+            else if(c == 'F') {
+                x += dirX[curDirIdx];
+                y += dirY[curDirIdx];
+
+                if(x == 0 && y == 0) break;
+            }
+        }
+
+        System.out.println(x == 0 && y == 0 ? time : -1);
+    }
+}
